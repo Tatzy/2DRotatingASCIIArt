481073883045ac876f57514642ca26c6f95a223e
Tatzy <108587201+Tatzy@users.noreply.github.com>
2025-03-18 19:17:17 +0100

    Fix inputting text files and add Circle.txt and Triangle.txt

--- README.md
+++ README.md
@@ -2,7 +2,5 @@
 Small program that simulates rotating objects in ASCII format on the command line
 
 ### How it works
-When starting the program, you'll be requested to input some type of String that will be used as the ASCII object for the rotation.    
-When no input is given the represented object will be a square. You can also input files via the command line, as an example:    
-"java RotatingObject < Thing.txt"    
-Formatting the ASCII input string properly, is still a work in progress, so there may still be some issues with that.   
+When starting the program, you'll be requested to input a file name which content will be used as the ASCII object for the rotation.    
+When no input is given the represented object will be a square. To use it type "java RotatingObject" into the command line.
--- RotatingObject.java
+++ RotatingObject.java
@@ -1,20 +1,21 @@
-import java.util.Arrays;
+
+import java.io.File;
 import java.util.Scanner;
 
 public class RotatingObject {
 
     public static void displayObject(char[][] arr) {
 
-        //final String PURPLE_FOREGROUND = "\u001b[38;5;129m";
+        // final String PURPLE_FOREGROUND = "\u001b[38;5;129m";
         final String RESET = "\u001b[39;49m";
         final String CLEAR = "\u001bc";
         final String MOVE = "\u001b[H";
 
         System.out.print(CLEAR);
 
-        //System.out.print(PURPLE_FOREGROUND);
-        for (int j = arr.length/3; j < arr.length*2/3; j++) {
-            for (int i = arr[0].length/3; i < arr[0].length*2/3; i++) {
+        // System.out.print(PURPLE_FOREGROUND);
+        for (int j = arr.length / 3; j < arr.length * 2 / 3; j++) {
+            for (int i = arr[0].length / 3; i < arr[0].length * 2 / 3; i++) {
                 // System.out.print(" "+arr[i][j]);
                 System.out.print("  " + arr[j][i]);
             }
@@ -24,49 +25,84 @@ public class RotatingObject {
 
     }
 
-    public static void inputObject(char[][] arr){
-        Scanner scanner = new Scanner(System.in);
-        int j = arr.length/3;
-        while (scanner.hasNextLine()&&j<arr.length*2/3) {
-            String line = scanner.nextLine();
-            char[] lineChar = line.toCharArray();
-            for (int i = 0; i < arr[0].length/3 && i<lineChar.length; i++) {
-                arr[j][i+arr.length/3] = lineChar[i];
+    public static void inputObject(char[][] arr, File file) {
+        try {
+            Scanner scanner = new Scanner(file);
+
+            int j = arr.length / 3;
+            while (scanner.hasNextLine() && j < arr.length * 2 / 3) {
+
+                String line = scanner.nextLine();
+                System.out.println(line);
+                char[] lineChar = line.toCharArray();
+                for (int i = 0; i < arr[0].length / 3 && i < lineChar.length; i++) {
+                    arr[j][i + arr.length / 3] = lineChar[i];
+                }
+                j++;
             }
-            j++;
+            scanner.close();
+        } catch (Exception e) {
+            System.out.println("Error");
+            System.exit(0);
         }
-        scanner.close();
+    }
+
+    public static int calcSize(File file){
+        int count = 0;
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                scanner.nextLine();
+                count++;
+            }
+            scanner.close();
+        } catch (Exception e) {
+            System.out.println("Error");
+            System.exit(0);
+        }
+        
+        return count;
     }
 
     public static void rotation2D(int sizing, int refreshTime, double angle, String input)
             throws InterruptedException {
-        int size = sizing;
-        //size for ASCII characters that are off screen;
-        char[][] object = new char[size][size];
+        
+        // size for ASCII characters that are off screen;
+        char[][] object;
+        int size;
 
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                object[i][j] = ' ';
+        
+
+        if (input.equals("") || input.equals("cube")) {
+            size = sizing;
+            object = new char[size][size];
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    object[i][j] = ' ';
+                }
             }
-        }
-
-        if (input.equals("")||input.equals("cube")) {
-            
             // creating square
-            for (int i = size * 4/ 9; i < size * 5 / 9; i++) {
-                object[i][size * 4/ 9] = '#';
-                object[i][size * 5/ 9] = '#';
-                object[size * 4/ 9][i] = '#';
-                object[size *  5/ 9][i] = '#';
+            for (int i = size * 4 / 9; i < size * 5 / 9; i++) {
+                object[i][size * 4 / 9] = '#';
+                object[i][size * 5 / 9] = '#';
+                object[size * 4 / 9][i] = '#';
+                object[size * 5 / 9][i] = '#';
             }
-        }
-        else{
-            inputObject(object);
+        } else {
+            File file = new File(input);
+            size = calcSize(file)*6;
+            object = new char[size][size];
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    object[i][j] = ' ';
+                }
+            }
+            inputObject(object, file);
         }
 
         displayObject(object);
         Thread.sleep(refreshTime);
-        
+
         char[][] newObject = new char[size][size];
         double[][] calcX = new double[size][size];
         double[][] calcY = new double[size][size];
@@ -87,10 +123,11 @@ public class RotatingObject {
                     double new_y = Math.round(x * Math.sin(radian) + (y) * Math.cos(radian)
                             - (size / 2 * Math.sin(radian) + size / 2 * Math.cos(radian)) + size / 2);
 
-                    if((new_x>=0&&new_y>=0&&new_x<size-1&&new_y<size-1)){
+                    if ((new_x >= 0 && new_y >= 0 && new_x < size - 1 && new_y < size - 1)) {
                         calcX[(int) (Math.round(size - 1 - new_y))][(int) (Math.round(new_x))] = new_x;
                         calcY[(int) (Math.round(size - 1 - new_y))][(int) (Math.round(new_x))] = new_y;
-                        newObject[(int) (Math.round(size - 1 - new_y))][(int) (Math.round(new_x))] = object[size - 1 - y][x];
+                        newObject[(int) (Math.round(size - 1 - new_y))][(int) (Math.round(new_x))] = object[size - 1
+                                - y][x];
                     }
 
                 }
@@ -121,16 +158,17 @@ public class RotatingObject {
                                 + (calcY[size - 1 - y][x]) * Math.cos(radian)
                                 - (size / 2 * Math.sin(radian) + size / 2 * Math.cos(radian)) + size / 2);
 
-                        if(new_x>=0&&new_y>=0&&new_x<size-1&&new_y<size-1){
-                        calcX[(int) (Math.round(size - 1 - new_y))][(int) (Math.round(new_x))] = new_x;
-                        calcY[(int) (Math.round(size - 1 - new_y))][(int) (Math.round(new_x))] = new_y;
-                        newObject[(int) (Math.round(size - 1 - new_y))][(int) (Math.round(new_x))] = object[size - 1 - y][x];
+                        if (new_x >= 0 && new_y >= 0 && new_x < size - 1 && new_y < size - 1) {
+                            calcX[(int) (Math.round(size - 1 - new_y))][(int) (Math.round(new_x))] = new_x;
+                            calcY[(int) (Math.round(size - 1 - new_y))][(int) (Math.round(new_x))] = new_y;
+                            newObject[(int) (Math.round(size - 1 - new_y))][(int) (Math.round(new_x))] = object[size - 1
+                                    - y][x];
                         }
                     }
                 }
             }
             Thread.sleep(refreshTime);
-            
+
             // copy
             for (int i = 0; i < size; i++) {
                 for (int j = 0; j < size; j++) {
@@ -142,31 +180,20 @@ public class RotatingObject {
 
     }
 
-    
     public static void main(String[] args) throws InterruptedException {
-        
-        
-        //Reading input for ascii document representing object
+
+        // Reading input for ascii document representing object
         Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Enter object in ASCII representation (default: cube)");
-        //First line has to be some type of string i.e. the name of the object
-        //if String is empty or "cube", display cube. After that, input the ascii representation of the object. Works best when the ascii representation of the object is big. For example:
-        //Triangle
-        //
-        //          #
-        //         # #
-        //        #   #
-        //       #     # 
-        //      #########
-        //
-        //Again, it is best if the representation of the triangle with ASCII letters is way bigger than in the example.
-        //Also the object needs to be centered in the document to spin around its axis 
-        String input=scanner.nextLine();
-        
-        //dimensions is only important for the size of the cube, input objects can not be automatically scaled
-        rotation2D(600, 100, 10, input);
-        //You might have to zoom out to get the whole display of the object
+
+        System.out.println("Enter name of text file in folder that gets displayed. For example: Triangle.txt");
+        // Input needs to be file name in same directory, for example: Triangle.txt. If input is empty or "cube", display cube. 
+        // Works best when the ascii representation of the object is big.
+        String input = scanner.nextLine();
         scanner.close();
+        // sizing is only important for the size of the example cube, input objects will automatically
+        // scale
+        rotation2D(600, 100, 10, input);
+        // You might have to zoom out to get the whole display of the object in the output console
+
     }
 }
